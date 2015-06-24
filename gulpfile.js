d95fcbfb4f41c4b420363b1c23a64c58ba848224
Mateu Adsuara <mateuadsuara@gmail.com>
2015-06-24 13:23:58 +0100

    Organises src and spec files in packages

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var concat = require('gulp-concat');
 
 gulp.task('test', function() {
   return gulp
-    .src(['vendor/*.js', 'src/*.js', 'spec/*.js'])
+    .src(['vendor/**/*.js', 'src/**/*.js', 'spec/**/*.js'])
     .pipe(jasmine({
       abortOnFail: true,
       integration: true
@@ -13,7 +13,7 @@ gulp.task('test', function() {
 
 gulp.task('compile', function() {
   return gulp
-    .src(['vendor/*.js', 'src/*.js'])
+    .src(['vendor/**/*.js', 'src/**/*.js'])
     .pipe(concat('tictactoe.js'))
     .pipe(gulp.dest('dist/'));
 });
